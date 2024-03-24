e0e2d7a951193a5b641d488cc35eb86ddc2de45b
suucong <sophia021210@naver.com>
2024-03-24 23:37:12 +0900

    feat: Add `3. 스프링 핵심 원리 이해2 - 객체지향원리 적용`

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,13 +1,23 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
+    @DisplayName("회원 가입")
     void join() {
         // given
         Member member = new Member(1L, "memberA", Grade.VIP);
