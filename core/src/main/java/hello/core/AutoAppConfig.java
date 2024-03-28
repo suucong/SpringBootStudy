4ae231be23bc4b93646c5f4f37d790a88fe6bdc3
suucong <sophia021210@naver.com>
2024-03-28 20:56:56 +0900

    feat: Add `6. 컴포넌트 스캔`

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+//        basePackages = , 를 지정해주지 않을 경우. 본인이 속한 패키지를 전부 뒤짐.
+//        -> 패키지를 지정하지 않고, 설정 정보 클래스의 위치를 프로젝트 최상단에 둔다.
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+}
